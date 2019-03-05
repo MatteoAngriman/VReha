55a88914ea4804cb7d6c7b6b87eee3c7f6b3bb46
MatteoAngriman <matteo.angriman@gmail.com>
2019-03-05 17:36:54 +0100

    Added waiting screen for VR

---
+++ Assets/Scripts/SceneData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Scene Data")]
+public class SceneData : ScriptableObject
+{
+    public int sceneID;
+    public int exerciseID;
+    public int patientID;
+    public float speed;
+    public bool isLeftHanded;
+
+
+
+}
