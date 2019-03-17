83ba3313962d75b0891e3742b579031b7ee4bfcf
MatteoAngriman <matteo.angriman@gmail.com>
2019-03-17 10:06:21 +0100

    Hands material
    .

---
+++ Assets/Scripts/Sc_2/HandColorChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandColorChanger : MonoBehaviour
+{
+    public GameObject[] left, right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        left = GameObject.FindGameObjectsWithTag("LeftHand");
+        right = GameObject.FindGameObjectsWithTag("RightHand");
+
+        foreach(GameObject l in left){
+            l.GetComponent<Material>().color = Color.red;
+        }
+        foreach(GameObject r in right)
+        {
+            r.GetComponent<Material>().color = Color.blue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
